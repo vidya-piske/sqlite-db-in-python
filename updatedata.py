2d31025d7e021ff9fb9c9b8ee77b3ef25fac63f9
vidya-piske <vidyapiske123@gmail.com>
2024-04-22 17:42:39 +0530

    updated the data in db

---
+++ updatedata.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# Connect to the SQLite database
+db = sqlite3.connect('restaurant_items.db')
+
+with db:
+    item_name = "updatedname"
+    id = 1
+
+    cur = db.cursor()
+
+    # Update the item_name column
+    cur.execute("UPDATE users SET item_name = ? WHERE id = ?", (item_name, id))
+
+    # Commit the transaction
+    db.commit()
+
+    print("Data updated")
